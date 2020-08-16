c233fce3072cd8e7ac6a5d2fd3349f897d577809
Charles McQuain <cmcquai1@my.westga.edu>
2020-08-16 15:21:45 -0700

    Added routes for task-list and add-task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'add-task', component: AddTaskComponent},
+  { path: 'task-list', component: TaskListComponent}
 ];
 
 @NgModule({
